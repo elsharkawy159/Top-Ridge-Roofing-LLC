9ba128a6b1c57059c24e0305a7bc2fab4c73003e
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-03-12 06:57:10 +0200

    Last

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
+var contactUsBtn = document.querySelectorAll(".contactUs")
+var nameInput = document.querySelector("#name"); 
+var emailInput = document.querySelector("#email");
+var phoneInput = document.querySelector("#phone");
+var subjInput = document.querySelector("#subj");
+var messageInput = document.querySelector("#message");
+var sendMsg = document.querySelector(".sendMsg");
 
 
+function sendData() {
+    if (nameInput.value == "" || emailInput.value == "" || phoneInput.value == "" || subjInput.value == "") {
+        document.querySelector(".fillForm").classList.remove("d-none");
+    } else {
+        document.querySelector(".fillForm").classList.add("d-none");
+
+            emailjs.send("service_29zq25s","template_0pan34o",{
+            from_name: "New User",
+            to_name: "Top Ridge Roofing LLC",
+            name: nameInput.value,
+            email: emailInput.value,
+            phone: phoneInput.value,
+            subject: subjInput.value,
+            message: messageInput.value,
+            
+    });
+            swal("Message Sent Successfully!", "Thank you", "success");
+            nameInput.value = "";
+            emailInput.value = "";
+            phoneInput.value = "";
+            subjInput.value = "";
+            messageInput.value = "";
+    }}
 (function () {
 
     /*=====================================
@@ -31,43 +61,25 @@
         }
         // show or hide the back-top-top button
         var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
             backToTo.style.display = "flex";
         } else {
             backToTo.style.display = "none";
         }
     };
-
-    var pageLink = document.querySelectorAll('.page-scroll');
-
-    pageLink.forEach(elem => {
-        elem.addEventListener('click', e => {
-            e.preventDefault();
-            document.querySelector(elem.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                offsetTop: 1 - 60,
-            });
-        });
-    });
-
     "use strict";
 
 }) ();
 
 window.addEventListener("load", ()=>{
-    $("body").fadeIn(300)
+    $(".header-content p").slideDown(500)
 })
-
-document.querySelector(".contactUs").addEventListener("click",()=>{
-    document.querySelector(".nameInput").focus();
-})
-
-function contact(){
-    document.querySelector(".nameInput").focus();
-
-}
-
-
+contactUsBtn.forEach(element => {
+    element.addEventListener("click",function() {
+    $(".contact-form-wrapper .section-title h2").css("display", "none")
+    $(".contact-form-wrapper .section-title h2").slideDown(1500)
+    })
+})  
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
@@ -77,7 +89,6 @@ var TxtType = function(el, toRotate, period) {
         this.tick();
         this.isDeleting = false;
     };
-
     TxtType.prototype.tick = function() {
         var i = this.loopNum % this.toRotate.length;
         var fullTxt = this.toRotate[i];
@@ -87,14 +98,10 @@ var TxtType = function(el, toRotate, period) {
         } else {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
-
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
         var that = this;
         var delta = 200 - Math.random() * 110;
-
         if (this.isDeleting) { delta /= 2; }
-
         if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
@@ -115,7 +122,7 @@ var TxtType = function(el, toRotate, period) {
             var toRotate = elements[i].getAttribute('data-type');
             var period = elements[i].getAttribute('data-period');
             if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
+            new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
         // INJECT CSS
